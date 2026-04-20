79b4daabdc4b5be2ba17d7cbdabe069a1b4e59e1
Małgorzata Jędruszek <275427@student.pwr.edu.pl>
2026-04-20 11:21:27 +0200

    Added saving images

--- subtract_images.py
+++ subtract_images.py
@@ -5,14 +5,17 @@ img2 = cv2.imread('images/cat5.png')
 
 #odejmowanie zwykłe
 subtraction = cv2.subtract(img1, img2)
-boosted_sub = cv2.multiply(subtraction, 5)
+boosted_sub = cv2.multiply(subtraction, 10)
 
 #różnica jako wartość bezwzględna
 diff = cv2.absdiff(img1, img2)
-boosted_diff = cv2.multiply(diff, 5)
+boosted_diff = cv2.multiply(diff, 10)
 
 cv2.imshow('Subtracted (img1 - img2)', boosted_sub)
 cv2.imshow('Absolute Difference', boosted_diff)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+cv2.imwrite('images/subcat15.png', boosted_sub)
+cv2.imwrite('images/abscat15.png', boosted_diff)
