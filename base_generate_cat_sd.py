02b4734629ac304295bb1805f29b226fd81c4f5e
Małgorzata Jędruszek <275427@student.pwr.edu.pl>
2026-06-14 08:39:51 +0200

    Added dog pics

--- base_generate_cat_sd.py
+++ base_generate_cat_sd.py
@@ -16,7 +16,7 @@ pipeline.enable_attention_slicing()
 # --- POMIJAMY ŁADOWANIE wag LoRA (dzięki temu mamy wersję fabryczną) ---
 
 # 2. Identyczny prompt dla sprawiedliwego porównania
-prompt = "a photo of a cat, high resolution, detailed masterpiece, cinematic lighting"
+prompt = "a photo of a dog, high resolution, detailed masterpiece, cinematic lighting"
 # Ustalamy stałe ziarno losowości, np. liczbę 42
 generator = torch.Generator("cpu").manual_seed(42)
 
@@ -24,5 +24,5 @@ generator = torch.Generator("cpu").manual_seed(42)
 image = pipeline(prompt, num_inference_steps=20, generator=generator).images[0]
 
 # 3. Zapisanie czystego kota pod inną nazwą
-image.save("kot_bazowy_lokalnie.png")
+image.save("pies_bazowy_lokalnie.png")
 print("Sukces! Czysty kot został zapisany jako 'czysty_kot_bazowy.png'")
